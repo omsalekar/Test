54f55ef31d7d9471aa573a8c961debe48bf3d12c
omsalekar <omsalekar6052@gmil.com>
2025-07-16 21:14:42 +0530

    Day one commit  adding project and one practice session

---
+++ DailySparks/Palindrome.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailySparks
+{
+    public class Palindrome
+    {
+        public void palindrome()
+        {
+            Console.WriteLine("Enter Your Input : ");
+            string Input = Console.ReadLine().ToLower();
+          
+            string Reverse = "";
+
+            for (int i = Input.Length-1; i >= 0; i--)
+            {
+                Reverse += Input[i];
+            }
+            if(Reverse == Input)
+            {
+                Console.WriteLine("Input is Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Input is not Palindrome");
+            }
+        }
+       
+
+
+    }
+}
