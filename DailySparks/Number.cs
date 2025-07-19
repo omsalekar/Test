112152c4b7ff950d4a17f910f5c9df16938f3f7e
omsalekar <omsalekar6052@gmil.com>
2025-07-19 16:47:32 +0530

    Adding new practice

--- DailySparks/Number.cs
+++ DailySparks/Number.cs
@@ -187,5 +187,8 @@ namespace DailySparks
             Console.WriteLine(reverse);
         
         }
+
+        public void num()
+       
     }
 }
---
+++ DailySparks/Valid_Paranthisis.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailySparks
+{
+    public class Valid_Paranthisis
+    {
+       
+            public bool IsValid(string s)
+            {
+                Stack<char> stack = new Stack<char>();
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+                    {
+                        stack.Push(s[i]);
+                    }
+                    else
+                    {
+                        if (stack.Count > 0 && (
+                        (stack.Peek() == '(' && s[i] == ')') ||
+                        (stack.Peek() == '{' && s[i] == '}') ||
+                        (stack.Peek() == '[' && s[i] == ']'))
+                        )
+                        {
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+
+                }
+                return stack.Count == 0;
+            }
+        }
+    }
+
