9cfccb7487a0c9a1e4a698bac1cec2cfff454cee
omsalekar <omsalekar6052@gmil.com>
2025-07-26 18:29:05 +0530

    Adding CodeWars Practice

--- DailySparks/Number.cs
+++ DailySparks/Number.cs
@@ -187,8 +187,23 @@ namespace DailySparks
             Console.WriteLine(reverse);
         
         }
+            public int RemoveElement(int[] nums, int val)
+            {
 
-        public void num()
-       
+                int k = 0;
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+
+                    if (nums[i] != val)
+                    {
+                        nums[k] = nums[i];
+                        k++;
+                    }
+                }
+                return k;
+            }
+        }
+
     }
 }
